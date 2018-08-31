67bd8c6cbcc900956426eb25d51a6013bb69c5fb
papery <dickequalle@googlemail.com>
2018-08-31 10:20:24 +0200

    create (placeMines) and print Board

--- minesweeper/include/Board.h
+++ minesweeper/include/Board.h
@@ -1,8 +1,50 @@
 #ifndef MINESWEEPER_BOARD_H
 #define MINESWEEPER_BOARD_H
 
+#include <iostream>
+#include <vector>
+#include <random>
+
+enum Status
+{
+    mine, revealed, unknown
+};
+
+struct Entry
+{
+    int data;
+    Status status;
+};
+
 class Board
 {
+private:
+
+    const int MINE = -1;
+
+    const int MAX_HEIGHT = 100;
+
+    const int MAX_WIDTH = 100;
+
+    int m_height;
+
+    int m_width;
+
+    int m_numMines;
+
+    std::vector<std::vector<Entry> > m_fields;
+
+    void placeMines();
+
+    void print(bool showAll);
+
+    void printEntryData(const Entry& entry);
+
+    void printEntryStatus(const Entry& entry);
+
+public:
+
+    Board(int height, int width, int numMines);
 
 };
 
--- minesweeper/src/Board.cpp
+++ minesweeper/src/Board.cpp
@@ -1,1 +1,129 @@
 #include "Board.h"
+
+Board::Board(const int height, const int width, const int numMines)
+{
+    m_height = std::min(height, MAX_HEIGHT);
+    m_width = std::min(width, MAX_WIDTH);
+    m_numMines = std::min(numMines, (int) (m_height * m_width * 0.5));
+
+    m_fields.resize(m_height);
+    for (std::vector<Entry>& line : m_fields)
+    {
+        line.resize(m_width, {0, Status::unknown});
+    }
+
+    placeMines();
+}
+
+void Board::placeMines()
+{
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> yDistribution(0, m_height - 1);
+    std::uniform_int_distribution<int> xDistribution(0, m_width - 1);
+
+    int placedMines = 0;
+
+    while (placedMines < m_numMines)
+    {
+        int y = yDistribution(generator);
+        int x = xDistribution(generator);
+
+        if (m_fields[y][x].data == MINE)
+        {
+            continue;
+        }
+
+        m_fields[y][x].data = MINE;
+        ++placedMines;
+
+        for (int adjY = y - 1; adjY <= y + 1; ++adjY)
+        {
+            if (adjY < 0 || adjY >= m_height)
+            {
+                continue;
+            }
+
+            for (int adjX = x - 1; adjX <= x + 1; ++adjX)
+            {
+                if (adjX < 0 || adjX >= m_width)
+                {
+                    continue;
+                }
+
+                if (m_fields[adjY][adjX].data != MINE)
+                {
+                    m_fields[adjY][adjX].data += 1;
+                }
+            }
+        }
+    }
+}
+
+void Board::print(bool showAll)
+{
+    std::cout << std::endl;
+
+    for (int y = -2; y < m_height; ++y)
+    {
+        for (int x = -2; x < m_width; ++x)
+        {
+            if ((y == -2 && x == -2) || y == -1 || x == -1)
+            {
+                std::cout << "   ";
+            }
+            else if (y == -2)
+            {
+                std::cout << x << " ";
+                if (x < 10)
+                {
+                    std::cout << " ";
+                }
+            }
+            else if (x == -2)
+            {
+                std::cout << y << " ";
+                if (y < 10)
+                {
+                    std::cout << " ";
+                }
+            }
+            else if (showAll)
+            {
+                printEntryData(m_fields[y][x]);
+            }
+            else
+            {
+                printEntryStatus(m_fields[y][x]);
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+void Board::printEntryData(const Entry& entry)
+{
+    if (entry.data == MINE)
+    {
+        std::cout << "x  ";
+    }
+    else
+    {
+        std::cout << entry.data << "  ";
+    }
+}
+
+void Board::printEntryStatus(const Entry& entry)
+{
+    if (entry.status == Status::unknown)
+    {
+        std::cout << "?  ";
+    }
+    else if (entry.status == Status::mine)
+    {
+        std::cout << "*  ";
+    }
+    else
+    {
+        printEntryData(entry);
+    }
+}
